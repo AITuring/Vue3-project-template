86e9fc444b68ef8321dcae10732b83082bccec75
AITuring <shspku365@gmail.com>
2021-04-26 02:09:14 +0800

    增加了axios相关配置

---
+++ src/utils/axios.ts
@@ -0,0 +1,47 @@
+import Axios from 'axios'
+import {ElMessage} from 'element-plus'
+
+const baseURL = 'https://api.github.com'
+
+const axios = Axios.create({
+    baseURL,
+    timeout: 20000 // 请求超时20s
+})
+
+// 前置拦截器(发起请求之前的拦截)
+axios.interceptors.request.use(
+    (response) => {
+        /**
+         * 根据实际情况对config做处理
+         * 这里没有做任何处理，直接返回了
+         */
+        return response
+    },
+    (error) => {
+        return Promise.reject(error)
+    }
+)
+
+// 后置拦截器(获取到响应时的拦截)
+axios.interceptors.response.use(
+    (response) => {
+        /**
+         * 根据实际情况对responce和error处理
+         * 此处没有处理，直接返回
+         */
+        return response
+    },
+    (error) => {
+        if (error.response && error.response.data) {
+            const code = error.response.status
+            const message = error.response.data.message
+            ElMessage.error(`Code: ${code}, Message: ${message}`)
+            console.error(`[Axios Error]`, error.response)
+        } else {
+            ElMessage.error(`${error}`)
+        }
+        return Promise.reject(error)
+    }
+)
+
+export default axios
